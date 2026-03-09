3b68d62e6ab6cb3668d030117a24c60767334ff1
vishalagrawal9696 <132067852+vishalagrawal9696@users.noreply.github.com>
2026-03-09 13:28:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int []{i,j};
                 }
             }
         }
-        // If no valid pair is found, return an empty array instead of null
-        return new int[] {};
+        return new int[]{};
     }
 }
